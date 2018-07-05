1bab3a5729dc29b355a69559b8f01d56dfe77257
Hisashi Miura <hisashi.office@gmail.com>
2018-07-05 12:27:23 +0900

    追加：phiyp 関数用テスト

--- shading_correction_factor/effect_coefficient_test.py
+++ shading_correction_factor/effect_coefficient_test.py
@@ -1,6 +1,7 @@
 import effect_coefficient
 
 import unittest
+import csv
 
 class TestCalcFaFunction(unittest.TestCase):
     
@@ -37,5 +38,20 @@ class TestCalcFaFunction(unittest.TestCase):
                 expected = fAA
                 self.assertAlmostEqual(actual,expected,delta=0.000000001)
 
+class TestCalcPhiypFunction(unittest.TestCase):
+    
+    # 正しい値が返ってくるかどうかのテスト    
+    def test_assert(self):
+        f = open('./test_case/phiyp.csv','r',encoding='utf8')
+        reader = csv.reader(f)
+        header = next(reader)
+        for i, row in enumerate(reader):
+            row_float = [float(d) for d in row]
+            case, X1, X2, X3, X1yp, X1ym, X3yp, X3ym, Y1, Y2, Y3, Y1xp, Y1xm, Y3xp, Y3xm, Zxp, Zxm, Zyp, Zym, phiypA = tuple(row_float)
+            WSSize = [X1, X2, X3, X1yp, X1ym, X3yp, X3ym, Y1, Y2, Y3, Y1xp, Y1xm, Y3xp, Y3xm, Zxp, Zxm, Zyp, Zym]
+            with self.subTest(case = case):
+                actual = effect_coefficient.calc_phiyp(WSSize)
+                expected = phiypA
+                self.assertAlmostEqual(actual,expected,delta=0.000000001)
 if __name__ == "__main__":
     unittest.main()
