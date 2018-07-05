f69f47be883e5a4a12616a1628af810402ddf7ee
Hisashi Miura <hisashi.office@gmail.com>
2018-07-05 16:15:03 +0900

    追加：sun_position 関数のテスト

---
+++ shading_correction_factor/sun_position_test.py
@@ -0,0 +1,35 @@
+import sun_position
+
+import unittest
+import csv
+
+class TestCalcNDayNHourFunction(unittest.TestCase):
+    
+    # 正しい値が返ってくるかどうかのテスト    
+    def test_assert(self):
+        f = open('./TestConfig01/NDayNHour.csv','r',encoding='utf8')
+        reader = csv.reader(f)
+        header = next(reader)
+        for i, row in enumerate(reader):
+            row_float = [float(d) for d in row]
+            Hour00, NdayA, NhourA = tuple(row_float)
+            with self.subTest(Hour00 = Hour00):
+                actual = sun_position.calc_NDayNHour(Hour00)
+                expected = (NdayA, NhourA)
+                self.assertAlmostEqual(actual[0], expected[0], delta=0.000000001)
+                self.assertAlmostEqual(actual[1], expected[1], delta=0.000000001)
+
+class TestCalcTT(unittest.TestCase):
+    
+    # 正しい値が返ってくるかどうかのテスト    
+    def test_assert(self):
+        NDT = 6
+        for NHour in range(0,24):
+            for MM in range(0,NDT):
+                actual = sun_position.calc_TT(NHour, NDT, MM)
+                expected = NHour + MM / 6
+                self.assertAlmostEqual(actual, expected, delta=0.000000001)
+                
+
+if __name__ == "__main__":
+    unittest.main()
