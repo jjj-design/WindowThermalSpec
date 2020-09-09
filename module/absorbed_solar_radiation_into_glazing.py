35bc6dd148001b4f961f3e5fc28e9576a661edc3
Hisashi Miura <hisashi.office@gmail.com>
2020-09-10 07:13:54 +0900

    [improve] 斜め入射の計算関係を整理する。

---
+++ module/absorbed_solar_radiation_into_glazing.py
@@ -0,0 +1,259 @@
+from collections import namedtuple
+import csv
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+import RollershadeAngularProperty as RAP
+import IncidentSolarRadiationOnGlazing as ISROG
+import DivisionDiffuseRatio as DDR
+
+from module import multiple_reflection as mr
+from module import glass_angular_property as gap
+
+# %% md
+
+# 各層の吸収日射量を計算するモジュール
+
+# %% md
+
+## 1. Functions
+
+# %% md
+
+開口面日射量（直達・天空・地表面反射）とグレージング複合体各層の日射吸収率から各層の吸収日射量を計算する。
+
+# %% md
+
+### 入力値
+
+$ I_
+{T, b} $ ：斜面（開口面）直達日射量(W / m < sup > 2 < / sup >)
+$ I_
+{T, d} $ ：斜面（開口面）天空日射量(W / m < sup > 2 < / sup >)
+$ I_
+{T, r} $ ：斜面（開口面）地表面反射日射量(W / m < sup > 2 < / sup >)
+$ \alpha_
+{glz, b, j} $ ：直達日射に対する層$j$の日射吸収率
+$ \alpha_
+{glz, d, j, l, m} $ ：天空上の微小要素からの入射日射に対する層$j$の日射吸収率
+$ \alpha_
+{glz, r, j, l, m} $ ：地表面上の微小要素からの入射日射に対する層$j$の日射吸収率
+$ F_
+{d, l, m} $ ：天空日射量に対する天空上の微小要素からの天空日射量の割合
+$ F_
+{r, l, m} $ ：地表面反射日射量に対する地表面上の微小要素からの地表面反射日射量の割合
+
+# %% md
+
+### 出力値
+
+$ I_
+{\alpha, j} $ ：層$j$での吸収日射量(W / m < sup > 2 < / sup >)
+
+# %% md
+
+### 計算方法
+
+#### データ構造と値の範囲
+
+$ \alpha_
+{glz, b, j} $・$ \alpha_
+{glz, d, j} $・$ \alpha_
+{glz, r, j} $ の組み合わせをクラス『ata＿input』とする。
+
+# %%
+
+class ata_input():
+    def __init__(self, absb, absd, absr):
+        self.absb = absb
+        self.absd = absd
+        self.absr = absr
+
+
+# absb：直達成分の日射吸収率
+# absd：天空成分の日射吸収率
+# absr：地表面反射成分の日射吸収率
+
+# %% md
+
+#### 層$j$での吸収日射量の計算
+
+層$j$での吸収日射量の計算方法を以下に示す。
+
+# %% md
+
+$$
+\begin
+{eqnarray}
+& \displaystyle
+I_
+{\alpha, j} = I_
+{T, b} \cdot \alpha_
+{glz, b, j} + I_
+{T, d} \cdot \alpha_
+{glz, d} + I_
+{T, r} \cdot \alpha_
+{glz, r} & \qquad\qquad\text
+{(1)} \nonumber\ \
+    \nonumber \ \
+    & \displaystyle \alpha_
+{glz, d} = \sum_
+{l = 0} ^ {N_
+{d, lat} - 1}\sum_
+{m = 0} ^ {N_
+{d, lon} - 1} F_
+{d, l, m} \cdot \alpha_
+{glz, d, j, l, m} & \qquad\qquad\text
+{(2)} \nonumber\ \
+    \nonumber \ \
+    & \displaystyle \alpha_
+{glz, r} = \sum_
+{l = 0} ^ {N_
+{r, lat} - 1}\sum_
+{m = 0} ^ {N_
+{r, lon} - 1} F_
+{r, l, m} \cdot \alpha_
+{glz, r, j, l, m} & \qquad\qquad\text
+{(3)} \nonumber\ \
+    \end
+{eqnarray}
+$$
+
+# %%
+
+# 層jの吸収日射量
+def absorb_sol(L, M):
+    return L.Itb * M.absb + L.Itd * M.absd + L.Itr * M.absr
+
+
+# 直達日射での層jの吸収率
+def actual_abs_beam(L, gl_type, phi_b):
+    ly_in_b = np.empty(len(L), dtype=tuple)
+    for j in range(0, len(L)):
+        if gl_type[j] == 0:  # ガラス
+            p = L[j].glass_ang_prop(phi=phi_b)
+            ly_in_b[j] = mr.SolarSpecSingleLayer(tau_f=p[0], tau_b=p[1], rho_f=p[2], rho_b=p[3])
+        elif gl_type[j] == 1:  # ロールスクリーン
+            ly_in_b[j] = mr.SolarSpecSingleLayer(
+                RAP.role_ang_prop(L[j], phi_b)[0],
+                RAP.role_ang_prop(L[j], phi_b)[1],
+                RAP.role_ang_prop(L[j], phi_b)[2],
+                RAP.role_ang_prop(L[j], phi_b)[3]
+            )
+
+    return mr.get_abs_multi_layer(ly_in_b)
+
+
+# 天空日射での層jの吸収率
+def actual_abs_diffuse(L, gl_type, Nd_lat, Nd_lon):
+    F_d = np.empty((Nd_lat, Nd_lon))
+    phi_d = np.empty((Nd_lat, Nd_lon))
+    abs_d = np.empty((len(L), Nd_lat, Nd_lon))
+    r_absd = np.zeros(len(L))
+    ly_in_d = np.empty(len(L), dtype=tuple)
+    for l in range(0, Nd_lat):
+        for m in range(0, Nd_lon):
+            dd_in = DDR.dd_input(Nd_lat, Nd_lon, l, m)
+            F_d[l][m] = DDR.division_diffuse_ratio(dd_in)[0]  # 割合
+            phi_d[l][m] = DDR.division_diffuse_ratio(dd_in)[3]  # 入射角
+
+            for j in range(0, len(L)):
+                if gl_type[j] == 0:  # ガラス
+                    p = L[j].glass_ang_prop(phi=phi_d[l][m])
+                    ly_in_d[j] = mr.SolarSpecSingleLayer(tau_f=p[0], tau_b=p[1], rho_f=p[2], rho_b=p[3])
+                elif gl_type[j] == 1:  # ロールスクリーン
+                    ly_in_d[j] = mr.SolarSpecSingleLayer(
+                        RAP.role_ang_prop(L[j], phi_d[l][m])[0],
+                        RAP.role_ang_prop(L[j], phi_d[l][m])[1],
+                        RAP.role_ang_prop(L[j], phi_d[l][m])[2],
+                        RAP.role_ang_prop(L[j], phi_d[l][m])[3]
+                    )
+
+            x = mr.get_abs_multi_layer(ly_in_d)
+            for j in range(0, len(L)):
+                abs_d[j][l][m] = x[j]
+                r_absd[j] += F_d[l][m] * abs_d[j][l][m]
+
+    return r_absd
+
+
+# 地表面反射日射での層jの吸収率
+def actual_abs_reflect(L, gl_type, Nr_lat, Nr_lon):
+    F_r = np.empty((Nr_lat, Nr_lon))
+    phi_r = np.empty((Nr_lat, Nr_lon))
+    abs_r = np.empty((len(L), Nr_lat, Nr_lon))
+    r_absr = np.zeros(len(L))
+    ly_in_r = np.empty(len(L), dtype=tuple)
+    for l in range(0, Nr_lat):
+        for m in range(0, Nr_lon):
+            dr_in = DDR.dr_input(Nr_lat, Nr_lon, l, m)
+            F_r[l][m] = DDR.division_reflect_ratio(dr_in)[0]  # 割合
+            phi_r[l][m] = DDR.division_reflect_ratio(dr_in)[3]  # 入射角
+
+            for j in range(0, len(L)):
+                if gl_type[j] == 0:  # ガラス
+                    p = L[j].glass_ang_prop(phi=phi_r[l][m])
+                    ly_in_r[j] = mr.SolarSpecSingleLayer(tau_f=p[0], tau_b=p[1], rho_f=p[2], rho_b=p[3])
+                elif gl_type[j] == 1:  # ロールスクリーン
+                    ly_in_r[j] = mr.SolarSpecSingleLayer(
+                        RAP.role_ang_prop(L[j], phi_r[l][m])[0],
+                        RAP.role_ang_prop(L[j], phi_r[l][m])[1],
+                        RAP.role_ang_prop(L[j], phi_r[l][m])[2],
+                        RAP.role_ang_prop(L[j], phi_r[l][m])[3]
+                    )
+
+            x = mr.get_abs_multi_layer(ly_in_r)
+            for j in range(0, len(L)):
+                abs_r[j][l][m] = x[j]
+                r_absr[j] += F_r[l][m] * abs_r[j][l][m]
+
+    return r_absr
+
+
+# %% md
+
+#### Example
+
+# %%
+
+if __name__ == '__main__':
+
+    # 面材の種類
+    gl_type = [0, 0, 1]  # 各層のグレージング種類（0：ガラス、1：ロールスクリーン、2：横型ブラインド、3：縦型ブラインド）
+
+    # 面材ごとの光学特性
+    gl_in = [
+        gap.GlassInput(trs_0_f=0.815, trs_0_b=0.815, ref_0_f=0.072, ref_0_b=0.072, gtype=0, c_type_f=False, c_type_b=False),
+        gap.GlassInput(trs_0_f=0.815, trs_0_b=0.815, ref_0_f=0.072, ref_0_b=0.072, gtype=0, c_type_f=False, c_type_b=False),
+        RAP.role_input(0.3, 0.3, 0.63, 0.63)
+    ]
+
+    # 斜面日射量
+    ita_in = ISROG.ita_input(2.6840261248 * 10. ** 6. / 3600., 0.1181932153 * 10. ** 6. / 3600.,
+                             0.094 * 10. ** 6. / 3600.)
+
+    # 直達日射の入射角
+    phi_b = 31.6403837639
+
+    # 天空分割数
+    Nd_lat = 90
+    Nd_lon = 180
+
+    # 地表面反射分割数
+    Nr_lat = 90
+    Nr_lon = 180
+
+    abs_b = np.zeros(len(gl_in))
+    abs_d = np.zeros(len(gl_in))
+    abs_r = np.zeros(len(gl_in))
+    I_abs = np.zeros(len(gl_in))
+    for j in range(0, len(gl_in)):
+        abs_b[j] = actual_abs_beam(gl_in, gl_type, phi_b)[j]
+        abs_d[j] = actual_abs_diffuse(gl_in, gl_type, Nd_lat, Nd_lon)[j]
+        abs_r[j] = actual_abs_reflect(gl_in, gl_type, Nr_lat, Nr_lon)[j]
+
+        ata_in = ata_input(abs_b[j], abs_d[j], abs_r[j])
+        I_abs[j] = absorb_sol(ita_in, ata_in)  # 斜面（開口面）吸収日射量
+
+    print(I_abs)
--- module/glass_angular_property.py
+++ module/glass_angular_property.py
@@ -1,80 +1,115 @@
 import math
+from typing import List
 
 
-class glass_input():
+class GlassInput:
 
-    def __init__(self, TRS0f, TRS0b, REF0f, REF0b, gtype, ctypef, ctypeb):
+    def __init__(
+            self,
+            trs_0_f: float,
+            trs_0_b: float,
+            ref_0_f: float,
+            ref_0_b: float,
+            gtype: int,
+            c_type_f: bool,
+            c_type_b: bool
+    ):
+        """
 
-        self.TRS0f = TRS0f
-        self.TRS0b = TRS0b
-        self.REF0f = REF0f
-        self.REF0b = REF0b
-        self.gtype = gtype
-        self.ctypef = ctypef
-        self.ctypeb = ctypeb
+        Args:
+            trs_0_f: 正面側からの入射光に対する垂直入射時の日射透過率
+            trs_0_b: 背面側からの入射光に対する垂直入射時の日射透過率
+            ref_0_f: 正面側からの入射光に対する垂直入射時の日射反射率
+            ref_0_b: 背面側からの入射光に対する垂直入射時の日射反射率
+            gtype: ガラスの種類flag（0：透明フロート板ガラス、1：Low-Eガラス）
+            c_type_f: 正面側の膜の有無（FALSE：膜なし、TRUE：膜あり）
+            c_type_b: 背面側の膜の有無（FALSE：膜なし、TRUE：膜あり）
+        """
 
-# TRS0f：正面側からの入射光に対する垂直入射時の日射透過率
-# TRS0f：正面側からの入射光に対する垂直入射時の日射透過率
-# TRS0b：背面側からの入射光に対する垂直入射時の日射透過率
-# REF0f：正面側からの入射光に対する垂直入射時の日射反射率
-# REF0b：背面側からの入射光に対する垂直入射時の日射反射率
-# gtype：ガラスの種類flag（0：透明フロート板ガラス、1：Low-Eガラス）
-# ctypef：正面側の膜の有無flag（0：膜なし、1：膜あり）
-# ctypeb：背面側の膜の有無flag（0：膜なし、1：膜あり）
+        self._trs_0_f = trs_0_f
+        self._trs_0_b = trs_0_b
+        self._ref_0_f = ref_0_f
+        self._ref_0_b = ref_0_b
+        self._gtype = gtype
+        self._c_type_f = c_type_f
+        self._c_type_b = c_type_b
 
+    def glass_ang_prop(self, phi: float):
+        """
+        入射角φの日射透過率及び日射反射率の計算
+        Args:
+            phi:入射角, degree
+        Returns:
 
-# 入射角φの日射透過率及び日射反射率の計算
-def glass_ang_prop(L, phi):
-    TRSnf = 0.
-    TRSnb = 0.
-    REFnf = 0.
-    REFnb = 0.
-    for i in range(0, 6):
-        TRSnf += glass_mTRS(L)[i] * math.cos(math.radians(phi)) ** i  # 規準化透過率(正面側)
-        TRSnb += glass_mTRS(L)[i] * math.cos(math.radians(phi)) ** i  # 規準化透過率(背面側)
-        if L.ctypef == 0:
-            REFnf += glass_mREFg(L)[i] * math.cos(math.radians(phi)) ** i  # 規準化反射率(正面側、膜なし)
-        elif L.ctypef == 1:
-            REFnf += glass_mREFc(L)[i] * math.cos(math.radians(phi)) ** i  # 規準化反射率(正面側、膜あり)
+        """
 
-        if L.ctypeb == 0:
-            REFnb += glass_mREFg(L)[i] * math.cos(math.radians(phi)) ** i  # 規準化反射率(背面側、膜なし)
-        elif L.ctypeb == 1:
-            REFnb += glass_mREFc(L)[i] * math.cos(math.radians(phi)) ** i  # 規準化反射率(背面側、膜あり)
+        # 斜め入射のコサイン
+        cos_phi = math.cos(math.radians(phi))
 
-    TRSf = L.TRS0f * TRSnf  # 入射角φの透過率(正面側)
-    TRSb = L.TRS0b * TRSnb  # 入射角φの透過率(背面側)
-    REFf = L.REF0f + (1 - L.REF0f) * REFnf  # 入射角φの反射率(正面側)
-    REFb = L.REF0b + (1 - L.REF0b) * REFnb  # 入射角φの反射率(背面側)
+        # 規準化透過率(正面側)
+        trs_n_f = sum([self._get_m_trs()[i] * cos_phi ** i for i in range(0, 6)])
 
-    return TRSf, TRSb, REFf, REFb
+        # 規準化透過率(背面側)
+        trs_n_b = sum([self._get_m_trs()[i] * cos_phi ** i for i in range(0, 6)])
 
+        # 規準化反射率(正面側)
+        ref_n_f = sum([self.get_m_ref(c_type=self._c_type_f)[i] * cos_phi ** i for i in range(0, 6)])
 
-# 係数mの選択。ここでは、透明フロート板ガラス及びLow-Eガラスのみを記述する。
-def glass_mTRS(L):
-    if L.gtype == 0:
-        return [0.000, 2.552, 1.364, -11.388, 13.617, -5.146]
-    elif L.gtype == 1:
-        return [0.000, 2.273, 1.631, -10.358, 11.769, -4.316]
-    else:
-        return ValueError
+        # 規準化反射率(背面側)
+        ref_n_b = sum([self.get_m_ref(c_type=self._c_type_b)[i] * cos_phi ** i for i in range(0, 6)])
 
+        # 入射角φの透過率(正面側)
+        trs_f = self._trs_0_f * trs_n_f
 
-def glass_mREFg(L):
-    if L.gtype == 0:
-        return [1.000, -5.189, 12.392, -16.593, 11.851, -3.461]
-    elif L.gtype == 1:
-        return [1.000, -5.084, 12.646, -18.213, 13.967, -4.316]
-    else:
-        return ValueError
+        # 入射角φの透過率(背面側)
+        trs_b = self._trs_0_b * trs_n_b
 
+        # 入射角φの反射率(正面側)
+        ref_f = self._ref_0_f + (1 - self._ref_0_f) * ref_n_f
 
-def glass_mREFc(L):
-    if L.gtype == 0:
-        return ValueError
-    elif L.gtype == 1:
-        return [1.000, -4.387, 9.175, -11.152, 7.416, -2.052]
-    else:
-        return ValueError
+        # 入射角φの反射率(背面側)
+        ref_b = self._ref_0_b + (1 - self._ref_0_b) * ref_n_b
 
+        return trs_f, trs_b, ref_f, ref_b
 
+    def _get_m_trs(self) -> List[float]:
+        """
+        規準化透過率の計算に用いる係数mを取得する。
+        Returns:
+            係数m
+        Notes:
+            ここでは、透明フロート板ガラス及びLow-Eガラスのみを記述する。
+        """
+
+        return {
+            0: [0.000, 2.552, 1.364, -11.388, 13.617, -5.146],
+            1: [0.000, 2.273, 1.631, -10.358, 11.769, -4.316]
+        }[self._gtype]
+
+    def get_m_ref(self, c_type: int) -> List[float]:
+        """
+        規準化反射率の計算に用いる係数mを取得する。
+        Args:
+            c_type: 膜の有無（あり=1, なし=0)
+        Returns:
+            係数m
+        Notes:
+            ここでは、透明フロート板ガラス及びLow-Eガラスのみを記述する。
+        """
+
+        if self._gtype == 0:
+
+            return [1.000, -5.189, 12.392, -16.593, 11.851, -3.461]
+
+        elif self._gtype == 1:
+
+            # 膜あり
+            if c_type:
+                return [1.000, -4.387, 9.175, -11.152, 7.416, -2.052]
+            # 膜なし
+            else:
+                return [1.000, -5.084, 12.646, -18.213, 13.967, -4.316]
+
+        else:
+
+            raise ValueError
